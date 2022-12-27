9a365c137d74e3ddce41c68ca9e803b549265367
Nuno Freire <greven.pt@gmail.com>
2022-12-27 20:24:57 +0000

    🚧

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: false,
+  htmlWhitespaceSensitivity: "ignore",
+  trailingComma: "es5",
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const plugin = require("tailwindcss/plugin")
+
+module.exports = {
+  plugins: [
+    plugin(function ({ addBase, addUtilities, addComponents, config }) {
+      // Add your custom styles here
+    }),
+  ],
+}
